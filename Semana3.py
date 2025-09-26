7a1a881f6979ccb4e9a47c4426fb609041b14263
berganzaarantza <berganza.arantza@gmail.com>
2025-09-25 23:41:59 -0600

    Semana3
    pt 1 y 2

---
+++ Semana3.py
@@ -0,0 +1,10 @@
+# Este programa fue hecho en Python
+var1 = 100
+var2 = 200
+var3 = var1 + var2
+print(var3)
+usuario = "alumno"
+print(usuario)
+"""En algunos casos, vamos a necesitar 
+crear comentarios "multilínea", de tal forma que
+nuestro yo del futuro sepa qué hicimos y por qué"""
---
+++ Semana3pt2.py
@@ -0,0 +1,31 @@
+year = 2025
+period = 5
+
+grad = year + period
+nineth = year - 3
+salary = year * 0.25
+test = year / 3
+
+comparacion1 = year > nineth #Creo yo que si
+comparacion2 = year < nineth #Creo yo que nel
+comparacion3 = comparacion1 + 10
+comparacion4 = comparacion2 + 10
+
+"""
+print(grad)
+print(nineth)
+print(salary)
+print(test)
+print(comparacion1)
+print(comparacion2)
+print(comparacion3)
+print(comparacion4)
+"""
+
+pi = 3.14159
+print(pi)
+pi_int = int(pi)
+print(pi_int)
+
+pi_dec = pi - pi_int
+print(pi_dec)
