f088d3757d6d55a225d509e9094c2cea54e437e9
berganzaarantza <berganza.arantza@gmail.com>
2025-10-01 16:30:44 -0600

    Semana4
    parte1

---
+++ Semana4.py
@@ -0,0 +1,31 @@
+usuario = "Alvin" # Tipo string
+cantidad_alumnos = 79
+media_edad = 18.236579
+monto_hope = 1234567.8901
+inversion_evento = -98765.21548
+
+"""print(type(cantidad_alumnos))
+print(type(media_edad))
+
+print(type(cantidad_alumnos) is int)
+print(type(media_edad) is int)
+
+print("El usuario es", usuario, "y tiene", cantidad_alumnos, "pajaritos en su aula")
+print("y la edad promedio de los pajaritos es", media_edad)
+
+print(f"El usuario es {usuario}")
+print(f"y en su aula hay {cantidad_alumnos - 4} pajaritos")
+print(f"con una edad promedio de {media_edad:.1f} años.")
+print(f"Los pajaritos colectaron ${monto_hope:,.2f} como un donativo")
+print(f"y la totalidad de gastos fue de ${abs(inversion_evento):,.2f} dolares.")
+
+print(type(usuario) is str)
+
+esta_lloviendo = False
+print(type(esta_lloviendo) is not bool)
+print(type(monto_hope) is not bool)"""
+
+nombre = "Camila"
+apellido = "Berganza"
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
