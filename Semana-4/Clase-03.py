b9b47165ae0c1f7b933def0e740b70cbdce3f672
berganzaarantza <berganza.arantza@gmail.com>
2025-10-07 09:04:06 -0600

    Clases
    actualizadas

---
+++ Semana-4/Clase-03.py
@@ -0,0 +1,19 @@
+"""ItemIngresar = input("Ingrese el item: ")
+print(f"El item ingresado es {ItemIngresar}")"""
+
+my_string = "ABCDEFCCC" # string, cadena de caracteres
+
+"""letra = int(input("Indique el indice de la letra a mostrar (del 0 al 4): "))
+print(my_string[letra])"""
+
+"""print(len(my_string))""" # Para saber el numero de elementos en my_string
+# print(my_string[1:5:2]) Para subsetting por slices
+
+#print("O" not in my_string)
+
+#print("J"*8)
+
+#print(min(my_string))
+#print(max(my_string))
+
+print(my_string.count("C"))
